0b962b7fb8d25d8a9af68df5d721e2af762fe645
Martin Myrseth <myme@novem.no>
2023-11-06 23:28:07 +0100

    flake: Add initial novem flake

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  description = "Novem flake";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    poetry2nix = {
+      url = "github:nix-community/poetry2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }@inputs:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        # Use `inputs` to avoid shadowing + infinite recursion
+        poetry2nix = inputs.poetry2nix.lib.mkPoetry2Nix { inherit pkgs; };
+        poetryEnv = poetry2nix.mkPoetryEnv {
+          projectDir = ./.;
+          preferWheels = true;
+        };
+      in {
+        checks = {
+          default = pkgs.runCommand "run-pytest" { } ''
+            ${poetryEnv}/bin/pytest ${self}
+            touch $out
+          '';
+        };
+
+        packages = {
+          # Main Novem Python app/package
+          novem = poetry2nix.mkPoetryApplication {
+            projectDir = self;
+            preferWheels = true;
+          };
+          default = self.packages.${system}.novem;
+        };
+
+        # Development env
+        # Enter using `nix develop` (or using `direnv`)
+        devShells.default = poetryEnv.env;
+      });
+}
