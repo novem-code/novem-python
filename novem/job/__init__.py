faf625d6011b7ec94f8fa89697855c2f663ff5ef
Sondov Engen <sen@novem.io>
2025-12-11 22:43:20 +0100

    tags: add cli support for tags

--- novem/job/__init__.py
+++ novem/job/__init__.py
@@ -53,7 +53,9 @@ class NovemJobAPI(NovemAPI):
 
         # get a list of valid properties
         props = [
-            x for x in dir(self) if x[0] != "_" and x not in ["data", "read", "delete", "write", "shared", "config"]
+            x
+            for x in dir(self)
+            if x[0] != "_" and x not in ["data", "read", "delete", "write", "shared", "config", "create"]
         ]
 
         for k, v in kwargs.items():
